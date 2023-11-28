d263dbfcbbd241dccfac9c8a393bf918d8d1ad5c
Nick Weiland <njweiland23@gmail.com>
2023-11-28 10:06:57 -0600

    deliverable 1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import './App.css';
 import Search from './components/Search'
 import NewStoreForm from './components/NewStoreForm';
 import StoreList from './components/StoreList';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [stores, setStores] = useState([])
+
+  useEffect(() => {
+    loadStores();
+  }, []);
+
+  const loadStores = () => {
+    fetch("http://localhost:8085/stores")
+      .then((r) => r.json())
+      .then((data) => setStores(data));
+  };
+
+
+
   return (
     <div className="main-container">
       <img src="/images/bobsburgers.png" />
       <h1>Neighbor Stores</h1>
       <Search />
       <NewStoreForm />
-      <StoreList />
+      <StoreList stores={stores}/>
     </div>
   );
 }
--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,31 +1,28 @@
-import React from "react"
-import Store from "./Store"
+import React, { useState, useEffect } from "react";
+import Store from "./Store";
 
-function StoreList() {
+function StoreList({stores}) {
+
+    
 
 
-    return(
-        <table>
-            <tbody>
-                <tr>
-                    <th className="row-name">
-                        Name
-                    </th>
-                    <th>
-                        Image
-                    </th>
-                    <th>
-                        Season
-                    </th>
-                    <th>
-                        Episode
-                    </th>
-                </tr>
-                {/** Render a list of <Store> components here. */}
-            </tbody>
-        
-        </table>
-    );
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th className="row-name">Name</th>
+          <th>Image</th>
+          <th>Season</th>
+          <th>Episode</th>
+        </tr>
+        {/** Render a list of <Store> components here. */}
+        {stores.map((store) => (
+        <Store key={store.id} name={store.name} image={store.image} season={store.season} episode={store.episode} />
+        ))}
+
+      </tbody>
+    </table>
+  );
 }
 
 export default StoreList;
